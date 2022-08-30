a924d49631b73500f9f9926fbcad01af8c02ef56
skys215 <skys215@gmail.com>
2022-08-31 02:00:51 +0800

    Localized blade table header

--- views/templates/scaffold/table/blade/header.blade.php
+++ views/templates/scaffold/table/blade/header.blade.php
@@ -1,1 +1,1 @@
-<th class="txt--center">{!! $fieldTitle !!}</th>
+<th class="txt--center">@if($config->options->localized) @@lang('models/{{ $config->modelNames->camelPlural }}.fields.{{ $fieldName }}') @else {!! $fieldTitle !!} @endif</th>
